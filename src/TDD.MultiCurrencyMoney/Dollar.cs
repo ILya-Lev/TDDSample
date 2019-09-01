a42239da70d159fe6c566add8c0d120ebbdca982
Illia Levandovskyi <lis2889@gmail.com>
2019-09-02 00:21:14 +0300

    equality method for dollar class implmented

--- src/TDD.MultiCurrencyMoney/Dollar.cs
+++ src/TDD.MultiCurrencyMoney/Dollar.cs
@@ -2,10 +2,25 @@
 {
     public class Dollar
     {
-        public int Amount { get; }
+        private int Amount { get; }
 
         public Dollar(int amount) => Amount = amount;
 
         public Dollar Times(int factor) => new Dollar(Amount * factor);
+
+        public override bool Equals(object other)
+        {
+            if (object.ReferenceEquals(this, other)) return true;
+            if (other == null) return false;
+
+            if (other is Dollar otherDollar)
+                return this.Amount == otherDollar.Amount;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
--- test/TDD.MultiCurrencyMoney.Tests/DollarTests.cs
+++ test/TDD.MultiCurrencyMoney.Tests/DollarTests.cs
@@ -9,11 +9,42 @@ namespace TDD.MultiCurrencyMoney.Tests
         public void Times_Scalar_ChangesAmount()
         {
             var five = new Dollar(5);
-            var product = five.Times(2);
-            product.Amount.Should().Be(10);
+            five.Times(2).Equals(new Dollar(10)).Should().BeTrue();
+            five.Times(3).Equals(new Dollar(15)).Should().BeTrue();
+        }
 
-            product = five.Times(3);
-            product.Amount.Should().Be(15);
+        [Fact]
+        public void Equals_AnotherInstanceOfTheSameValue_True()
+        {
+            var five = new Dollar(5);
+            var anotherFive = new Dollar(5);
+
+            five.Equals(anotherFive).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_AnotherInstanceOfDifferentValue_False()
+        {
+            var five = new Dollar(5);
+            var six = new Dollar(6);
+
+            five.Equals(six).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_TheSameInstance_True()
+        {
+            var five = new Dollar(5);
+
+            five.Equals(five).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_ArbitraryObject_False()
+        {
+            var five = new Dollar(5);
+
+            five.Equals(new object()).Should().BeFalse();
         }
     }
 }
