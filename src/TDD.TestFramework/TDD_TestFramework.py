3adba1afcb97a222cda1da58cf7765f12cc992b8
ILya Lev <lis2889@gmail.com>
2019-09-11 11:49:40 +0200

    setUp method added

--- src/TDD.TestFramework/TDD_TestFramework.py
+++ src/TDD.TestFramework/TDD_TestFramework.py
@@ -2,14 +2,23 @@ class TestCase :
 	def __init__(self, name):
 		self.methodUnderTheTest = name
 	def run(self):
+		self.setUp()
+		#if hasattr(self, "setUp"):	#as TestCaseTest inherits TestCase - it does not contain the method so far
+		#	settingUp = getattr(self, "setUp")
+		#	settingUp()
+
 		method = getattr(self, self.methodUnderTheTest)
 		method()
+
+	def setUp(self): pass
 
 #was run class inherits test case class
 class WasRun(TestCase) : 
 	def __init__(self, name):
-		self.wasRun = None
 		TestCase.__init__(self, name)		#why calling base ctor is done after body of derived one?
 	def testMethod(self): 
 		self.wasRun = 1
+	def setUp(self):
+		self.wasRun = None
+		self.wasSetUp = 1
 	
--- src/TDD.TestFramework/Tests.py
+++ src/TDD.TestFramework/Tests.py
@@ -2,12 +2,19 @@ from TDD_TestFramework import WasRun
 from TDD_TestFramework import TestCase
 
 class TestCaseTest(TestCase):
+	def setUp(self):
+		self.test = WasRun("testMethod")
+
 	def testRunning(self):
-		test = WasRun("testMethod")
-		assert (not test.wasRun)
-		test.run()
-		assert (test.wasRun)
+		self.test.run()
+		assert (self.test.wasRun)
+	
+	def testSetUp(self):
+		self.test.run()
+		assert (self.test.wasSetUp)
+
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
